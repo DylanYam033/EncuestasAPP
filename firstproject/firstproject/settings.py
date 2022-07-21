2ed999bd28ef27b0447249ee964a69363b62a18d
Dylan Yampuezan <yampuezandylan04@gmail.com>
2022-07-21 16:44:51 -0500

    update

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.app.PollsConfig", #aplicacion personalizada creada por mi 
+    'polls', #aplicacion personalizada creada por mi
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
