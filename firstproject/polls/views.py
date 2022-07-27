2a9d07539c24062e153eedd131776a35f0b64485
Dylan Yampuezan <yampuezandylan04@gmail.com>
2022-07-27 14:08:26 -0500

    Creando las views y las rutas

--- firstproject/polls/views.py
+++ firstproject/polls/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def index(request): #vista basada en funcion, las views pueden estar basadas en funciones o clases. 
     return HttpResponse("Hello world")
+
+def detail(request, question_id):
+    """details of every question"""
+    return HttpResponse(f'Estás viendo la pregunta {question_id}')
+
+def results(request, question_id):
+    """details of every question result"""
+    return HttpResponse(f'Estás viendo los resultados de la pregunta {question_id}')
+
+def vote(request, question_id):
+    """Functions for voting confirmation"""
+    return HttpResponse(f'Estás votando a la pregunta {question_id}')
     
     
