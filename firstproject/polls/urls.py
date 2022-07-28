5120f45e5550ad13802c4e259df402a4bf6ac0d3
Dylan Yampuezan <yampuezandylan04@gmail.com>
2022-07-28 17:03:57 -0500

    update

--- firstproject/polls/urls.py
+++ firstproject/polls/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = "polls" #nombre de la app para referenciar los archivos
 urlpatterns = [
     #se llaman las views creadas en views, las cuales son funciones
-    path("", views.index, name="index"),
-    path("<int:question_id>/detail/nea/wow", views.detail, name="detail"), 
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("", views.IndexView.as_view(), name="index"), #cuando son views basadas en clases se pone el nombre de la view y el .as_view()
+    path("<int:pk>/detail/nea/wow", views.DetailView.as_view(), name="detail"), 
+    path("<int:pk>/results/", views.ResultView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"), 
-    #con el nmame y el app name vamos a referenciar conectar el template con la views por medio del metodo de url
+    #con el name y el app name vamos a referenciar conectar el template con la views por medio del metodo de url
 ]
--- firstproject/polls/views.py
+++ firstproject/polls/views.py
@@ -3,29 +3,28 @@ from audioop import reverse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic #importo las views generic
 
 #importo el modelo de question 
 from .models import Question, Choice
 
-#Las views son el backend de nuestra app, las cuales van a estar ligadas a un template(front)
-def index(request): #vista basada en funcion, las views pueden estar basadas en funciones o clases. 
-    latest_question_list = Question.objects.all #objeto queryset (conjuntos) con las preguntas
-    return render(request, "polls/index.html", { #render lleva tres parametros: request, ruta del template y un contexto(diccionario)
-        "latest_question_list": latest_question_list #dejamos disponible la variable para ser usada en index.html
-    })
+class IndexView(generic.ListView): #vista basada en clases 
+    #atributos de la clase
+    template_name = "polls/index.html" #ruta del template
+    context_object_name = "latest_question_list" 
 
-def detail(request, question_id):
-    question = get_object_or_404 (Question, pk = question_id) #arroja el error 404
-    return render(request, "polls/detail.html", { #render lleva tres parametros: request, ruta del template y un contexto(diccionario)
-        "question": question #dejamos disponible la variable para ser usada en index.html
-    })
-    
+    def get_queryset(self): #metodo de la clase IndexView
+        """Return the last published questions"""
+        return Question.objects.order_by("-pub_date")
 
-def results(request, question_id):
-    question =get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", { #render lleva tres parametros: request, ruta del template y un contexto(diccionario)
-        "question": question 
-    })
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html" #ruta del template
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html" #ruta del template
+
 
 def vote(request, question_id): # recibe dos parametros que vienen de detail.html
     question =get_object_or_404(Question, pk=question_id) #recibe el modelo Question y la pk de la question
@@ -40,3 +39,23 @@ def vote(request, question_id): # recibe dos parametros que vienen de detail.htm
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,))) #cuando vota lo rederigimos a otra template
 
+
+#Las views son el backend de nuestra app, las cuales van a estar ligadas a un template(front)
+# def index(request): #vista basada en funcion, las views pueden estar basadas en funciones o clases. 
+#     latest_question_list = Question.objects.all #objeto queryset (conjuntos) con las preguntas
+#     return render(request, "polls/index.html", { #render lleva tres parametros: request, ruta del template y un contexto(diccionario)
+#         "latest_question_list": latest_question_list #dejamos disponible la variable para ser usada en index.html
+#     })
+
+# def detail(request, question_id):
+#     question = get_object_or_404 (Question, pk = question_id) #arroja el error 404
+#     return render(request, "polls/detail.html", { #render lleva tres parametros: request, ruta del template y un contexto(diccionario)
+#         "question": question #dejamos disponible la variable para ser usada en index.html
+#     })
+    
+
+# def results(request, question_id):
+#     question =get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", { #render lleva tres parametros: request, ruta del template y un contexto(diccionario)
+#         "question": question 
+#     })
