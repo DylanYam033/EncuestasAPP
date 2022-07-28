18e6c8a6a3ef99dcea17525030b004dc529a604b
Dylan Yampuezan <yampuezandylan04@gmail.com>
2022-07-28 09:24:58 -0500

    update

--- firstproject/polls/urls.py
+++ firstproject/polls/urls.py
@@ -6,7 +6,7 @@ app_name = "polls" #nombre de la app para referenciar los archivos
 urlpatterns = [
     #se llaman las views creadas en views, las cuales son funciones
     path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="index"), 
-    path("<int:question_id>/results/", views.results, name="index"),
-    path("<int:question_id>/vote/", views.vote, name="index"),
+    path("<int:question_id>/detail/nea", views.detail, name="detail"), 
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
