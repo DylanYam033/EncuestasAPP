e6207e6fedd24ca79a626bfb04a81525b7411a3a
Dylan Yampuezan <yampuezandylan04@gmail.com>
2022-07-28 11:16:05 -0500

    update

--- firstproject/polls/models.py
+++ firstproject/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 #Al hacer el mapeo Django nos pasa a tablas nuestras clases 
 
@@ -7,8 +9,16 @@ class Question(models.Model):
     question_text = models.CharField(max_length=100) #se indica que el atributo sera un varchar en la base de datos
     pub_date = models.DateField("date published") #se indica que el atributo sera tipo date en la base de datos 
 
+    def __str__(self): #funcion para mostrar las preguntas y no el object
+    	return self.question_text
+    
+    
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) #se indica que este atributo es llave foranea, o sea la llave primaria de question
     choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0) #se indica que este atributo es de tipo int con valor 0 por defecto
+
+    def __str__(self):
+        return self.choice_text
+
     
--- firstproject/polls/urls.py
+++ firstproject/polls/urls.py
@@ -6,7 +6,8 @@ app_name = "polls" #nombre de la app para referenciar los archivos
 urlpatterns = [
     #se llaman las views creadas en views, las cuales son funciones
     path("", views.index, name="index"),
-    path("<int:question_id>/detail/nea", views.detail, name="detail"), 
+    path("<int:question_id>/detail/nea/", views.detail, name="detail"), 
     path("<int:question_id>/results/", views.results, name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("<int:question_id>/vote/", views.vote, name="vote"), 
+    #con el nmame y el app name vamos a referenciar conectar el template con la views por medio del metodo de url
 ]
