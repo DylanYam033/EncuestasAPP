3712c438454b53e43a5a3d672c813de9c21ed6cc
Dylan Yampuezan <yampuezandylan04@gmail.com>
2022-08-25 13:27:06 -0500

    update

--- firstproject/polls/admin.py
+++ firstproject/polls/admin.py
@@ -5,7 +5,7 @@ class ChoiceInline(admin.StackedInline): #agregar respuestas a las preguntas con
     model = Choice #Asigno el modelo de donde se va a desplegar 
     extra = 3 #Numero de respuestas por pregunta
 
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(admin.ModelAdmin): #en esta clase puedo poner funcionalidades al admin de django usando los atributos de nuestro modelo
     fields = ["question_text", "pub_date"] #defino el orden de los campos (atributos) del modelo a llenar
     inlines = [ChoiceInline] #agrego la clase que me permite agregar respuestas
     list_display = ["question_text", "pub_date"]
